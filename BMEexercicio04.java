cc01bba5c929ab5ab363655d0b8dfd29c928a1ee
KarlAlonne <87619289+KarlAlonne@users.noreply.github.com>
2022-05-12 00:03:49 -0300

    Create BMEexercicio04.java

---
+++ BMEexercicio04.java
@@ -0,0 +1,8 @@
+class BMEexercicio04 {
+  public static void main(String[] args){
+    long long1 = 100;
+    int int1 = 200;
+    int int2 = int1 + (int) long1;
+    System.out.println(int2);
+  }
+}
